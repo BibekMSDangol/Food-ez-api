4636623514d9a5bb1336212a115163bfe8de9ee4
BibekMSDangol <78782357+BibekMSDangol@users.noreply.github.com>
2022-12-20 16:43:03 +0545

    middleware created and necessary libraries installed

--- index.js
+++ index.js
@@ -2,10 +2,23 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+//Connect to MongoDB
 mongoose.set('strictQuery', true);
 mongoose.connect('mongodb://127.0.0.1:27017/FE').then(()=>{
     console.log('Connected to MongoDB Database Server')
 }).catch((err)=> console.log(err))
+
+
+//Application level middleware
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path}`);
+    next();
+  });
+
+//In-built middleware
+app.use(express.json());
+
+//Router level middleware
 
 app.use(login);
 
@@ -13,6 +26,11 @@ app.use(login);
 function login(req, res,next){
     res.send("Server is working")
 }
+//Error handling middleware
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    res.status(500).json({ err: err.message });
+  });
 
 app.listen(3000, ()=>{
     console.log('App is runing on port 3000.')
--- routes/user-router.js
+++ routes/user-router.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
 
 router.post("/register", (req, res, next) => {
@@ -10,24 +11,53 @@ router.post("/register", (req, res, next) => {
         let err = new Error(`Username ${req.body.username} already taken.`);
         return next(err);
       }
-      bcrypt.hash(req.body.password, 10, (err, hash)=>{
-        if(err) return next(err)
-        user = new User()
-        user.username = req.body.username
-        user.password = hash
-        user.save().then(user=>{
+      bcrypt.hash(req.body.password, 10, (err, hash) => {
+        if (err) return next(err);
+        user = new User();
+        user.username = req.body.username;
+        user.password = hash;
+        user
+          .save()
+          .then((user) => {
             res.status(201).json({
-                status: 'User registration successful',
-                userId: user._id,
-                username: user.username
-            })
-        }).catch(next)
-      })
-    }).catch(next);
+              status: "User registration successful",
+              userId: user._id,
+              username: user.username,
+            });
+          })
+          .catch(next);
+      });
+    })
+    .catch(next);
 });
 
 router.post("/login", (req, res, next) => {
-  res.send("login request");
+  User.findOne({ username: req.body.username }).then((user) => {
+    if (user == null) {
+      let err = new Error('User is not registered.');
+      return next(err);
+    }
+    bcrypt.compare(req.body.password, user.password,
+      (err, success)=>{
+        if(err) return next(err)
+        if(!success){
+          let err = new Error('Password doesnt match.');
+          return next(err)
+        }
+        let data ={
+          userId: user._id,
+          username: user.username
+        }
+        jwt.sign(data, process.env.SECRET, {expiresIn: '4d'},
+        (err, token)=>{
+          if(err) return next (err)
+          res.json({
+            status: 'Login Successful',
+            token: token
+          })
+        })
+      })
+  }).catch(next)
 });
 
 module.exports = router;
