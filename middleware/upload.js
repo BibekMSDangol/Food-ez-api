f0a7cac1eb87bae5328c779458f1866d4cb82206
BibekMSDangol <78782357+BibekMSDangol@users.noreply.github.com>
2023-01-08 20:34:36 +0545

    upload middleware updated

--- middleware/upload.js
+++ middleware/upload.js
@@ -9,3 +9,18 @@ const storage = multer.diskStorage({
         cb(null, `${file.fieldname}-${Date.now()}${ext}`)
     }
 })
+
+const imageFileFilter = (req, file, cb) =>{
+    if(!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)){
+        return cb(new Error('File format not supported.'), false)
+    }   
+    cb(null, true)
+}
+
+const uploadImage = multer({
+    storage: storage,
+    fileFilter: imageFileFilter,
+    limits: 2 * 1024 * 1024
+})
+
+module.exports = uploadImage
